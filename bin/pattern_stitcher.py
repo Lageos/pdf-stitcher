3bee4490dda4ff185dd848018aaa50672d8927d0
Michael Rußwurm <m.russwurm@gmail.com>
2016-01-06 18:43:10 +0100

    stitching rows works, line still overlap

--- bin/pattern_stitcher.py
+++ bin/pattern_stitcher.py
@@ -15,6 +15,8 @@ def addright(page, right_page, tx=0):
 
 def main():
     print('## Pattern Stitcher ##')
+    tx = 0
+    ty = 0
     with open(sys.argv[1], 'rb') as input:
         pdf = PyPDF2.PdfFileReader(input)
         print("Number of Pages: %1.2i" % pdf.getNumPages())
@@ -25,20 +27,32 @@ def main():
             rows = []
             for row in reader:
                 for page in row:
+                    print("Reading page %1.2i" % int(page))
                     if first_page is True:
-                        output = pdf.getPage(int(page))
+                        output = pdf.getPage(int(page) - 1)
                         first_page = False
                     else:
-                        output = addright(output, pdf.getPage(int(page)))
+                        output = addright(output, pdf.getPage(int(page) - 1), tx)
                 rows.append(output)
-            output = rows[0]
-            for row in rows:
-                output.mergeTranslatedPage(row, 0, output.mediaBox[3], expand=True)
+                first_page = True
 
+            print("Stitching lines of pages ...")
+            first_line = True
+            for row in rows:
+                if first_line is True:
+                    output = rows[0]
+                    print(output.mediaBox)
+                    first_line = False
+                else:
+                    print(output.mediaBox)
+                    output.mergeTranslatedPage(row, ty, -output.mediaBox[3], expand=True)
+
+        print("Saving new file...")
         with open(sys.argv[3], 'wb') as out_file:
             pdf_out = PyPDF2.PdfFileWriter()
             pdf_out.addPage(output)
             pdf_out.write(out_file)
+    print("Finished.")
     return
 
 if __name__ == "__main__":
